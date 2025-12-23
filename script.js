113429d5e20951c2d658b7e271bf3ffe3bf51a78
waynemldz <waynegabriel13@gmail.com>
2025-12-22 22:52:50 -0300

    adjusts

--- script.js
+++ script.js
@@ -16,14 +16,26 @@ function addTask() {
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
-    inputBox.value = "oi"
+    inputBox.value = ""
+    saveData();
 }
 
 listContainer.addEventListener("click", (e)=>{
   if(e.target.tagName === "LI"){
     e.target.classList.toggle("checked");
+    saveData();
   }else if(e.target.tagName === "SPAN"){
       e.target.parentElement.remove()
+      saveData();
     }
   
 }, false)
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask();
