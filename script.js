f4475fdb0482bce6baafb91eceae8fb048fb1b75
waynemldz <waynegabriel13@gmail.com>
2026-01-05 21:17:23 -0300

    task Editor added

--- script.js
+++ script.js
@@ -1,65 +1,86 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container")
-
+const listContainer = document.getElementById("list-container");
 
 // Add task
 
 function addTask() {
   if (inputBox.value === "") {
     alert("You must write something!");
-  }else{
+  } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
-
-     // Task editor
-
-    let editorDiv = document.createElement("div");
-    li.appendChild(editorDiv);
-
-    let inputEdit = document.createElement("input");
-    inputEdit.setAttribute("type", "text");
-    inputEdit.value = li.innerHTML;
-    editorDiv.appendChild(inputEdit)
 
     // Remove button
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
-    
   }
-    inputBox.value = ""
+  inputBox.value = "";
+  saveData();
+}
+
+// Task editor
+
+function taskEditor(e) {
+  const li = e.target;
+
+  if (li.querySelector(".task-editor")) {
+    return;
+  }
+
+  let editorDiv = document.createElement("div");
+  editorDiv.className = "task-editor";
+  li.appendChild(editorDiv);
+
+  let inputEdit = document.createElement("input");
+  inputEdit.type = "text";
+  inputEdit.value = li.firstChild.textContent;
+  editorDiv.appendChild(inputEdit);
+
+  let okButton = document.createElement("button");
+  okButton.innerText = "Ok";
+  editorDiv.appendChild(okButton);
+
+  okButton.addEventListener("click", () => {
+    li.firstChild.textContent = inputEdit.value;
+    editorDiv.remove();
     saveData();
+  });
 }
 
 // Checked and remove task
 
-listContainer.addEventListener("click", (e)=>{
-  if(e.target.tagName === "LI"){
-    e.target.classList.toggle("checked");
-    saveData();
-  }else if(e.target.tagName === "SPAN"){
-      e.target.parentElement.remove()
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      taskEditor(e);
+      // e.target.classList.toggle("checked");
+      // saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
       saveData();
     }
-  
-}, false)
+  },
+  false
+);
 
 // Save and show task
 
-function saveData(){
+function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
-  listContainer.innerHTML = localStorage.getItem("data")
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
 
 // Enter to add task
 
 inputBox.addEventListener("keydown", (e) => {
-  if(e.key === "Enter"){
-    addTask()
+  if (e.key === "Enter") {
+    addTask();
   }
-})
+});
